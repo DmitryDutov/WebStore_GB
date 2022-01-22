97108d7a4a6ff3aeee1ad04394cdd99373f1fe44
DmitryDutov <dmitry_dutov@inbox.ru>
2022-01-22 15:53:49 +0300

    Добавили webAPI контролле в основное приложение + добавили view для WebAPI

---
+++ UI/WebStore/Controllers/WebAPIController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.TestAPI;
+
+namespace WebStore.Controllers;
+public class WebAPIController:Controller
+{
+    private readonly IValuesService _ValueService;
+
+    public WebAPIController(IValuesService ValueService) => _ValueService = ValueService;
+
+    public IActionResult Index()
+    {
+        var values = _ValueService.GetValues();
+        return View(values);
+    }
+}
