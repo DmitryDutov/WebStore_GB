57bda2d5fccd591533781ad1c2d3cd685f54bb20
DmitryDutov <dmitry_dutov@inbox.ru>
2022-01-22 22:26:44 +0300

    Регистрация сервисов бизнес-логики в проекте хоста WebAPI

---
+++ Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/employees")] //заменили стандартный адрес на более удобный
+    public class EmployeesApiController : ControllerBase
+    {
+
+    }
+}
