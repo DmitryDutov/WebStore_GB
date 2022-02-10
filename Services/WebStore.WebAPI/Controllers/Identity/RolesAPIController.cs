a459aab3ad8c99da5b4b9fb608318a811ad4eaa9
DmitryDutov <dmitry_dutov@inbox.ru>
2022-02-10 20:52:13 +0300

    Добавлены методы в контроллеры User и Role WebAPI

--- Services/WebStore.WebAPI/Controllers/Identity/RolesAPIController.cs
+++ Services/WebStore.WebAPI/Controllers/Identity/RolesAPIController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces;
 
 namespace WebStore.WebAPI.Controllers.Identity;
@@ -7,4 +11,14 @@ namespace WebStore.WebAPI.Controllers.Identity;
 [Route(WebAPIAdresses.Identity.Roles)]
 public class RolesAPIController : ControllerBase
 {
+    private readonly RoleStore<Role> _RoleStore;
+    public RolesAPIController(WebStoreDB db)
+    {
+        _RoleStore = new RoleStore<Role>(db);
+        //_RoleStore = new(db);
+    }
+
+    [HttpGet("all")]
+    public async Task<IEnumerable<Role>> GetAll() => await _RoleStore.Roles.ToArrayAsync();
 }
+
--- Services/WebStore.WebAPI/Controllers/Identity/UsersAPIController.cs
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersAPIController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces;
 
-namespace WebStore.WebAPI.Controllers.Identity
+namespace WebStore.WebAPI.Controllers.Identity;
+
+[ApiController]
+[Route(WebAPIAdresses.Identity.Users)]
+public class UsersAPIController : ControllerBase
 {
-    [ApiController]
-    [Route(WebAPIAdresses.Identity.Users)]
-    public class UsersAPIController : ControllerBase
+    ////Если ничего не определено, то можно имспользовать так:
+    //private readonly UserStore _UserStore;
+    //public UsersAPIController(WebStoreDB db)
+    //{
+    //    _UserStore = new UserStore(db);
+    //}
+
+    private readonly UserStore<User, Role, WebStoreDB> _UserStore;
+    public UsersAPIController(WebStoreDB db)
     {
+        _UserStore = new UserStore<User, Role, WebStoreDB>(db); //можно так
+                                                                //_UserStore = new(db); //или так
+        _UserStore.AutoSaveChanges = true; //можно не прописывать
     }
+
+    [HttpGet("all")]
+    public async Task<IEnumerable<User>> GetAll() => await _UserStore.Users.ToArrayAsync();
 }
+
