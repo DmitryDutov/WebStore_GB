761327b8323e486e07e79a4b4edc1d664a46cb67
DmitryDutov <dmitry_dutov@inbox.ru>
2022-02-11 18:08:45 +0300

    Реализация Dispose шаблона

--- Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
+++ Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -3,7 +3,7 @@ using System.Net.Http.Json;
 
 namespace WebStore.WebAPI.Clients.Base;
 
-public abstract class BaseClient
+public abstract class BaseClient : IDisposable
 {
     protected HttpClient Http { get; }
     protected string Addres { get; }
@@ -59,5 +59,27 @@ public abstract class BaseClient
     }
 
     #endregion
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this); //если есть финализатор
+    }
+    //~BaseClient() => Dispose(false); //финализотор?
+
+    protected bool _Disposed;
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_Disposed ) return;
+        _Disposed = true;
+
+        if (disposing)
+        {
+            //освобождаем управляемые ресурсы - обычные объекты с интерфейсом IDispisible
+            //Http.Dispose();//не должны вызывать Disposr(), так как не мы его создавали
+        }
+
+        //освобождаем неуправляемые ресурсы (COM-объекты и т.п.)
+    }
 }
 
