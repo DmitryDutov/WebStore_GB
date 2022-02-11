ba05242b66005a7328176f5f4759e520e6fdc47f
DmitryDutov <dmitry_dutov@inbox.ru>
2022-02-11 17:49:58 +0300

    Работающие контроллеры пользователей и ролей

--- Common/WebStore.Domain/DTO/Identity/ClaimDTO.cs
+++ Common/WebStore.Domain/DTO/Identity/ClaimDTO.cs
@@ -5,21 +5,15 @@ namespace WebStore.Domain.DTO.Identity;
 public abstract class ClaimDTO : UserDTO
 {
     public IEnumerable<Claim> Claims { get; set; }
-
 }
 
-public class AddClaimDTO : ClaimDTO
-{
+public class AddClaimDTO : ClaimDTO { }
 
-}
-
-public class RemoveClaimDTO : ClaimDTO
-{
-
-}
+public class RemoveClaimDTO : ClaimDTO { }
 
 public class ReplaceClaimDTO : UserDTO
 {
     public Claim Claim { get; set; }
     public Claim NewClaim { get; set; }
 }
+
